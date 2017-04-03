513c80882d3e168771c1d5ceb3d35d4ecddfe7ba
shiramir <shiramiremail@gmail.com>
2017-04-03 19:43:36 +0300

    Full Screen GUI
    First version of fullscreen GUI, only functionality is loading files and
    directories. Update of false labeling zebra images.

--- segmentation/src/gui.py
+++ segmentation/src/gui.py
@@ -1,16 +1,14 @@
 from tkinter import *
-from tkinter import ttk
 from tkinter.filedialog import askopenfilename
 from tkinter.filedialog import askdirectory
 
-# define callback functions
-def get_train_img():
+def get_train():
     train_img_path.set(askopenfilename())
     
-def get_segment_img():
+def get_segment():
     labels_img_path.set(askopenfilename())
 
-def get_new_img():
+def get_new():
     test_img_path.set(askopenfilename())
 
 def get_output():
@@ -20,14 +18,16 @@ def compute():
     # the segmentation implementation goes here
     return image
 
-# define root and main-frame of the GUI
 root = Tk()
 root.title("Segmentation")
-frame = ttk.Frame(root)
-frame.grid(column=0, row=0, sticky=(N, W, E, S))
-frame.columnconfigure(0, weight=1)
-frame.rowconfigure(0, weight=1)
-frame.rowconfigure(0, weight=1)
+root.attributes("-fullscreen", True)
+root.configure(background='midnight blue')
+w, h = root.winfo_screenwidth(), root.winfo_screenheight()
+root.overrideredirect(1)
+root.geometry("%dx%d+0+0" % (w, h))
+
+root.focus_set() # <-- move focus to this widget
+root.bind("<Escape>", lambda e: e.widget.quit())
 
 # define all the variables
 image = []
@@ -36,26 +36,35 @@ labels_img_path = StringVar()
 test_img_path = StringVar()
 output_dir = StringVar()
 
-# define teh button widgets
-train_img_btn = ttk.Button(frame, text="Train Image", command=get_train_img, width=15)
-train_img_btn.grid(column=0, row=1, sticky=W)
-segment_img_btn = ttk.Button(frame, text="Segment Image", command=get_segment_img, width=15)
-segment_img_btn.grid(column=0, row=2, sticky=W)
-test_img_btn = ttk.Button(frame, text="New Image", command=get_new_img, width=15)
-test_img_btn.grid(column=0, row=3, sticky=W)
-output_path_btn = ttk.Button(frame, text="Output", command=get_output, width=15)
-output_path_btn.grid(column=0, row=4, sticky=W)
-run_btn = ttk.Button(frame, text="GO!", command=compute, width=15)
-run_btn.grid(column=0, row=5, sticky=W)
+headline = Label(root, font=(None, 20), text="Segmentation App", anchor=CENTER).grid(row=0, column=0, columnspan=7)
 
-# define the labels for debugging
-ttk.Label(frame, textvariable=train_img_path).grid(column=1, row=1, sticky=(W, E))
-ttk.Label(frame, textvariable=labels_img_path).grid(column=1, row=2, sticky=(W, E))
-ttk.Label(frame, textvariable=test_img_path).grid(column=1, row=3, sticky=(W, E))
-ttk.Label(frame, textvariable=output_dir).grid(column=1, row=4, sticky=(W, E))
+btn1 = Button(root, command=get_train, text="Train Image", width=15)
+btn1.grid(row=1, column=0, padx = 20, pady = 10)
+btn2 = Button(root, command=get_segment, text="Labeled Image", width=15)
+btn2.grid(row=2, column=0, padx = 20, pady = 10)
+btn3 = Button(root, command=get_new, text="Test Image", width=15)
+btn3.grid(row=3, column=0, padx = 20, pady = 10)
+btn4 = Button(root, command=get_output, text="Output Path", width=15)
+btn4.grid(row=4, column=0, padx = 20, pady = 10)
+btn5 = Button(root, command=compute, text="GO!", width=15)
+btn5.grid(row=5, column=0, padx = 20, pady = 10)
 
-# change appearnace
-for child in frame.winfo_children(): child.grid_configure(padx=5, pady=5)
+image_frame = Frame(bg="black", width=0.75*w, height=0.75*h).grid(row=1, column=1, columnspan=5, rowspan=20, sticky=E)
 
-# execute GUI application
+btn1 = Button(root, command=get_train, text="Train Image", width=15)
+btn1.grid(row=1, column=6, padx = 20, pady = 10)
+btn2 = Button(root, command=get_segment, text="Labeled Image", width=15)
+btn2.grid(row=2, column=6, padx = 20, pady = 10)
+btn3 = Button(root, command=get_new, text="Test Image", width=15)
+btn3.grid(row=3, column=6, padx = 20, pady = 10)
+btn4 = Button(root, command=get_output, text="Output Path", width=15)
+btn4.grid(row=4, column=6, padx = 20, pady = 10)
+btn5 = Button(root, command=compute, text="GO!", width=15)
+btn5.grid(row=5, column=6, padx = 20, pady = 10)
+
+# Label(root, textvariable=train_img_path).grid(column=1, row=0) 
+# Label(root, textvariable=labels_img_path).grid(column=1, row=1)
+# Label(root, textvariable=test_img_path).grid(column=1, row=2)
+# Label(root, textvariable=output_dir).grid(column=1, row=3)
+
 root.mainloop()
