53766952d4e32abc59d665cfca4cb52026855511
shiramir <shiramiremail@gmail.com>
2017-04-04 22:25:17 +0300

    Restoring GUI's Window Decorations
    Enables Closing with 'X' Button and toggling between other screens.

--- segmentation/src/gui.py
+++ segmentation/src/gui.py
@@ -48,10 +48,8 @@ def compute():
 
 root = Tk()
 root.title("Segmentation")
-root.attributes("-fullscreen", True)
 root.configure(background='#ccffcc')
 w, h = root.winfo_screenwidth(), root.winfo_screenheight()
-root.overrideredirect(1)
 root.geometry("%dx%d+0+0" % (w, h))
 
 root.focus_set() # <-- move focus to this widget
