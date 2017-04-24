73efc01f8025fd1d4f3f7b8186e5e7016c7e0be8
shiramir <shiramiremail@gmail.com>
2017-04-24 14:24:17 +0300

    More Refactoring
    Adding the rest of the DB

---
+++ facialRecognition/src/test.py
@@ -0,0 +1,33 @@
+# *************************************************
+# ******** Facial Detection and Recognition *******
+# ************ Merav Joseph 200652063 *************
+# ************* Shir Amir 209712801 ***************
+# *************************************************
+
+import numpy as np
+import cv2
+
+path1 = "utility\\haarcascade_frontalface_default.xml"
+path2 = "utility\\haarcascade_eye.xml"
+
+face_cascade = cv2.CascadeClassifier(path1)
+eye_cascade = cv2.CascadeClassifier(path2)
+
+img_name = 'harry_potter.jpg'
+img = cv2.imread(img_name)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+for (x, y, w, h) in faces:
+    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    roi_gray = gray[y:y+h, x:x+w]
+    roi_color = img[y:y+h, x:x+w]
+    eyes = eye_cascade.detectMultiScale(roi_gray)
+    for (ex, ey, ew, eh) in eyes:
+        cv2.rectangle(roi_color, (ex,ey), (ex+ew,ey+eh), (0, 255, 0), 2)
+
+cv2.imshow('face decetion',img)
+cv2.imwrite('%s_res.jpg' % (img_name), img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
