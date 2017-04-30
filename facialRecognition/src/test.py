38cc9eb2d329f5f0002227f801ca4125a08e2be6
shiramir <shiramiremail@gmail.com>
2017-04-30 11:58:11 +0300

    PCAProject is Working!!!
    Finally managed to project an image over a set of eigenfaces.

--- facialRecognition/src/test.py
+++ facialRecognition/src/test.py
@@ -14,22 +14,24 @@ img = cv2.imread(img_name)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 faces = ef.FACE_CASCADE.detectMultiScale(gray, 1.3, 5)
 eigen_files = os.listdir('../eigenfaces')
+eigen_files = eigen_files[0:1]
+
+faces_mat = np.zeros((1, ef.IMG_LENGTH))
 
 for (x, y, w, h) in faces:
     cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
     roi_gray = gray[y:y+h, x:x+w]
     resized_img = cv2.resize(roi_gray, (ef.SIZE_X, ef.SIZE_Y), interpolation=cv2.INTER_LINEAR)
+    np.vstack((faces_mat, resized_img.reshape(1, ef.IMG_LENGTH)))
     roi_color = img[y:y+h, x:x+w]
     eyes = ef.EYE_CASCADE.detectMultiScale(roi_gray)
 
     for (ex, ey, ew, eh) in eyes:
-        cv2.rectangle(roi_color, (ex,ey), (ex+ew,ey+eh), (0, 255, 0), 2)
-    for f in eigen_files:
-        eigenvecs = np.genfromtxt('../eigenfaces/%s' % f, delimiter=',')
-        mean = eigenvecs[0]
-        print(np.shape(eigenvecs))
-        eigenvecs = np.delete(eigenvecs, 0, 0)
-        print(np.shape(eigenvecs))
-        print(np.shape(resized_img.reshape(1, ef.IMG_LENGTH)))
-        img_reconstruction = cv2.PCAProject(resized_img.reshape(1, ef.IMG_LENGTH), mean, eigenvecs)
+        cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
 
+for f in eigen_files:
+    eigenvecs = np.genfromtxt('../eigenfaces/%s' % f, delimiter=',')
+    mean = eigenvecs[0]
+    mean = mean.reshape((1,ef.IMG_LENGTH))
+    eigenvecs = np.delete(eigenvecs, 0, 0)
+    img_reconstruction = cv2.PCAProject(faces_mat, mean, eigenvecs)
