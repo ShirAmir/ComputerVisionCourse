28b8b5ffaa255cd717e1c831b539968ad5beea58
shiramir <shiramiremail@gmail.com>
2017-05-19 12:41:56 +0300

    One Contour Per Object
    Appling threshold on the contour size to achieve one contour per object.

--- bugTracker/src/tracker.py
+++ bugTracker/src/tracker.py
@@ -5,9 +5,13 @@
 # *************************************************
 
 import numpy as np
+import numpy.random as rnd
 import cv2
 
-cap = cv2.VideoCapture('../videos/bugs14.mp4')
+VIDEO = r'../videos/bug00.mp4'
+CONTOUR_SIZE_THRESH = 150
+
+cap = cv2.VideoCapture(VIDEO)
 
 fgbg = cv2.bgsegm.createBackgroundSubtractorMOG()
 
@@ -17,13 +21,16 @@ while(True):
 
     # Our operations on the frame come here
     fgmask = fgbg.apply(frame)
-    im2, contours, hierarchy = cv2.findContours(fgmask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    _, contours, hierarchy = cv2.findContours(fgmask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    cv2.drawContours(frame, contours, -1, (255, 0, 0), 1, 8)
+    for i in range(len(contours)):
+        if cv2.contourArea(contours[i]) >= CONTOUR_SIZE_THRESH:
+            clr = (rnd.randint(255), rnd.randint(255), rnd.randint(255))
+            cv2.drawContours(frame, contours, i, clr, 2, 8)
 
     # Display the resulting frame
     cv2.imshow('frame',frame)
-    if cv2.waitKey(30) & 0xFF == ord('q'):
+    if cv2.waitKey(200) & 0xFF == ord('q'):
         break
 
 # When everything done, release the capture
