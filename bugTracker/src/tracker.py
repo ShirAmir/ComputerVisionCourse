6663009da1d14dfb14192c2e5e9ffe97f6c680be
shiramir <shiramiremail@gmail.com>
2017-05-21 15:48:20 +0300

    One Kalman Filter
    The app has one working Kalman Filter.

--- bugTracker/src/tracker.py
+++ bugTracker/src/tracker.py
@@ -10,27 +10,48 @@ import cv2
 
 VIDEO = r'../videos/bug00.mp4'
 CONTOUR_SIZE_THRESH = 150
+RECT_WIDTH = 40
+RECT_HEIGHT = 30
 
-cap = cv2.VideoCapture(VIDEO)
-
+video = cv2.VideoCapture(VIDEO)
 fgbg = cv2.bgsegm.createBackgroundSubtractorMOG()
+kalman = cv2.KalmanFilter(4,2)
+kalman.measurementMatrix = np.array([[1,0,0,0],[0,1,0,0]],np.float32)
+kalman.transitionMatrix = np.array([[1,0,1,0],[0,1,0,1],[0,0,1,0],[0,0,0,1]],np.float32)
+kalman.processNoiseCov = np.array([[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]],np.float32) * 0.03
 
 while(True):
     # Capture frame-by-frame
-    ret, frame = cap.read()
+    ret, frame = video.read()
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-    # Our operations on the frame come here
+    #Apply Background Subtraction
     fgmask = fgbg.apply(frame)
+    #Find object contours
     _, contours, hierarchy = cv2.findContours(fgmask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     for i in range(len(contours)):
         if cv2.contourArea(contours[i]) >= CONTOUR_SIZE_THRESH:
             clr = (rnd.randint(255), rnd.randint(255), rnd.randint(255))
             cv2.drawContours(frame, contours, i, clr, 2, 8)
+            #Find contour's centriod
+            moment = cv2.moments(contours[i])
+            cx = int(moment['m10']/moment['m00'])
+            cy = int(moment['m01']/moment['m00'])
+            # for debug -- show centriod
+            cv2.circle(frame, (cx,cy), 3, (255, 0, 0), -1)
+            measure = np.array([[np.float32(cx)],[np.float32(cy)]])
+            kalman.correct(measure)
+            pred = kalman.predict()
+            cx , cy = pred[0], pred[1]
+            pt1 = (cx + int(RECT_HEIGHT / 2), cy - int(RECT_WIDTH / 2))
+            pt2 = (cx - int(RECT_HEIGHT / 2), cy + int(RECT_WIDTH / 2))
+            cv2.rectangle(frame, pt1, pt2, (0,0,0), 2, 8)
+            break
 
     # Display the resulting frame
-    cv2.imshow('frame',frame)
-    if cv2.waitKey(200) & 0xFF == ord('q'):
+    cv2.imshow('frame', cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+    if cv2.waitKey(100) & 0xFF == ord('q'):
         break
 
 # When everything done, release the capture
